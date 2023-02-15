ba9f372010ff500bb2873188b8e5c22fa0232c1f
WJY1204 <gina911204z@gmail.com>
2023-02-16 06:44:16 +0800

    Add Animation

--- Assets/playerAnimation.cs
+++ Assets/playerAnimation.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class playerAnimation : MonoBehaviour
 {
+    private Animator playerAnimator;
+
+    public string currentState;
+    public string playerIdle = "playerIdle";
+    public string playerWalk = "playerWalk";
+    public string playerJump = "playerJump";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerVariables.isWalking && !playerVariables.isJumping)
+        {
+            PlayAnim(playerJump);
+        }
     }
+
+    public void PlayAnim(string newState){
+        if (currentState == newState) return;
+        playerAnimator.Play(newState);
+        currentState = newState;
+    }
+
 }
