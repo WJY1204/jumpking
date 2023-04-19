17c8287648d096005d820908692ca502614a5d5a
WJY <gina911204z@gmail.com>
2023-04-20 04:18:34 +0800

    420finsh

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Goal : MonoBehaviour
 {
     public int unlockNextLevel = 0;
     
+    public TMP_Text time;
+    public GameObject goalCanvas;
+
+    public TimerMachine timerMachine;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            timerMachine.Pause();
+            time.text = (Mathf.Round(timerMachine.timer * 100f) / 100f).ToString();
             PlayerPrefs.SetInt("lastUnlockPos", unlockNextLevel);
-            SceneTransitionManager.TriggerSceneTransition("Menu", 1f);
+            goalCanvas.SetActive(true); //開啟介面
         }
     }
+
+    public void SceneTransit()
+    {
+        SceneTransitionManager.TriggerSceneTransition("Menu", 1f);
+    }
+
 }
