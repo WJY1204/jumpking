983f72d010ef4c3d7424446ac4fa0f4c790647af
WJY1204 <gina911204z@gmail.com>
2023-05-30 05:46:15 +0800

    fixed player color and add video

---
+++ Assets/Scripts/VideoPlayerEndingHandler.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerEndingHandler : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    public string animationName;
+    public string nextLevel;
+
+    private void Start()
+    {
+        // Subscribe to the video player's loop point reached event
+        videoPlayer.loopPointReached += OnVideoPlayerEnd;
+    }
+
+    private void OnVideoPlayerEnd(VideoPlayer vp)
+    {
+        if (vp == videoPlayer)
+        {
+            // The video has ended, implement your desired logic here
+            Debug.Log("Video ended!");
+
+            // Example: Load a new scene or display a UI element
+            // SceneManager.LoadScene("NextScene");
+            // endScreenUI.SetActive(true);
+            SceneTransitionManager.TriggerSceneTransition(nextLevel, 1f);
+
+            PlayerPrefs.SetInt(animationName, 1);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe from the video player's loop point reached event
+        videoPlayer.loopPointReached -= OnVideoPlayerEnd;
+    }
+}
